8757f47f21f4b6ec1580db260d57850eb0c007a9
riyazclock <42862680+riyazclock@users.noreply.github.com>
2019-02-07 12:20:04 +0530

    Update vowelsorconsonent.py

--- vowelsorconsonent.py
+++ vowelsorconsonent.py
@@ -1,6 +1,10 @@
 r=['a','e','i','o','u','A','E','I','O','U']
+d=['!','@','#','$','%','^','&','*']
 ch=str(input("enter the character"))
 if(ch in r):
   print("vowels")
+elif(ch in d):
+  print("invalid")  
 else:
   print("consonent")
+ 
