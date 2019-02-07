be623a1a92f29868a22ee8e60b672b0eeeb9b1bb
riyazclock <42862680+riyazclock@users.noreply.github.com>
2019-02-07 13:59:57 +0530

    Create alp.py

---
+++ alp.py
@@ -0,0 +1,6 @@
+d=['a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','q','w','e','r','t','y','u','i','o','p','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
+a=input()
+if (a in d):
+  print("Alphabet")
+else:
+  print("No")  
