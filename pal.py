c416e0a18ceed959f7b76dbf064867af753ad2a2
riyazclock <42862680+riyazclock@users.noreply.github.com>
2019-02-19 12:26:48 +0530

    Update pal.py

--- pal.py
+++ pal.py
@@ -1,10 +1,10 @@
-num = int(input())
-temp = num
+no = int(input())
+temp = no
 rev = 0
 while temp != 0:
 	rev = (rev * 10) + (temp % 10)
 	temp = temp//10
-if num == rev:
+if no == rev:
 	print("yes")
 else:
 	print("no")
